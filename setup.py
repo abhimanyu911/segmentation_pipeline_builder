b3f1b39553f7e71d914fa88d66f4f16718231f34
Abhimanyu Borthakur <abhimanyuborthakur@hotmail.com>
2021-11-02 23:06:07 +0530

    update

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ classifiers = [
 
 setup(
   name='dataset_builder_for_segmentation',
-  version='0.0.2',
+  version='0.0.3',
   description='A dataset pipeline builder for semantic image segmentation',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',  
+  url='https://github.com/abhimanyu911/segmentation_pipeline_builder',  
   author='Abhimanyu Borthakur',
   author_email='abhimanyuborthakur@hotmail.com',
   license='MIT', 
